2bd01549a711d3cf67397e875e419a8d1767cad2
akhilpamwani <83491855+akhilpamwani@users.noreply.github.com>
2022-09-17 12:01:18 +0530

    first commit

--- next.config.js
+++ next.config.js
@@ -2,6 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
 
+}
+module.exports = {
+  reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_CONTACT_API_URL: `https://codehustler.herokuapp.com/ContactData`,
+    NEXT_PUBLIC_BLOG_API_URL: `https://codehustle.herokuapp.com/BlogData`,
+    NEXT_PUBLIC_BLOG_READ_API_URL: `https://codehustler.herokuapp.com/BlogRead/`,
+    NEXT_PUBLIC_BLOG_SINGLE_API_URL: 'https://codehustler.herokuapp.com/BlogReadOne',
+    
+  },
+  }
 module.exports = nextConfig
